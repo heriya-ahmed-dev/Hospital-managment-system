270950e8a2be71352366d898ad0f8a3de9aeae06
Heracitta <heriyaahmedsiraje@gmail.com>
2026-08-31 15:24:36 +0300

    Add docs connected to backend

--- src/Componenets/Doctors/AddDoctors.jsx
+++ src/Componenets/Doctors/AddDoctors.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState,useeffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 const AddDoctors = () => {
@@ -26,7 +26,7 @@ const AddDoctors = () => {
 
     const handleSubmit = async(e) =>{
         e.preventDefault();
-        axios.post('',addDocs);
+        await axios.post('http://localhost:5500/AddDocs',addDocs);
         setAddDocs({
              image: '',
              firstname:'',
@@ -39,21 +39,22 @@ const AddDoctors = () => {
              department:'',
              action:''
         })
+        alert('Data succesfully sent to data base')
         navigate('/home')
     }
 
     
   return (
-    <form onClick={handleSubmit} className='bg-[#D3D3D3] h-screen'>
+    <form onSubmit={handleSubmit} className='bg-[#D3D3D3] h-screen'>
            <h4 className='flex justify-center p-2'>Add Doctors</h4>
            <div className='flex justify-center gap-[120px]'>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>First Name</p>
-                    <input className='bg-[white] w-[280px] h-[32px]' name='firstname' value={addDocs.firstname} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px] h-[32px] ps-3' name='firstname' value={addDocs.firstname} onChange={handleChange}/>
                 </div>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>last Name</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='lastname' value={addDocs.lastname} onChange={handleChange} />
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='lastname' value={addDocs.lastname} onChange={handleChange} />
                 </div>  
            </div>
 
@@ -63,47 +64,47 @@ const AddDoctors = () => {
                 </div>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Email</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='email' value={addDocs.email} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='email' value={addDocs.email} onChange={handleChange}/>
                 </div>  
            </div>
 
             <div className='flex justify-center gap-[120px]'>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Date of Birth</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='dob' value={addDocs.dob} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='dob' value={addDocs.dob} onChange={handleChange} type='date'/>
                 </div>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Gender</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' value={addDocs.gender} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='gender' value={addDocs.gender} onChange={handleChange}/>
                 </div>  
            </div>
 
             <div className='flex justify-center'>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1 flex justify-center'>Address</p>
-                    <input className='bg-[white] w-[680px]  h-[32px]' name='address' value={addDocs.address} onChange={handleChange}/>
+                    <input className='bg-[white] w-[680px]  h-[32px] ps-3' name='address' value={addDocs.address} onChange={handleChange}/>
                 </div> 
            </div>
 
             <div className='flex justify-center gap-[120px]'>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Phone</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='phone' value={addDocs.phone} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='phone' value={addDocs.phone} onChange={handleChange}/>
                 </div>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Image</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='image' value={addDocs.image} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='image' value={addDocs.image} onChange={handleChange} type='file'/>
                 </div>  
            </div>
 
             <div className='flex justify-center gap-[120px]'>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Department</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='department' value={addDocs.department} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='department' value={addDocs.department} onChange={handleChange}/>
                 </div>
                 <div className='my-1'>
                     <p className='ps-4 text-[18px] my-1'>Action</p>
-                    <input className='bg-[white] w-[280px]  h-[32px]' name='action' value={addDocs.action} onChange={handleChange}/>
+                    <input className='bg-[white] w-[280px]  h-[32px] ps-3' name='action' value={addDocs.action} onChange={handleChange}/>
                 </div>  
            </div>
            <div className='flex justify-center my-3'>
